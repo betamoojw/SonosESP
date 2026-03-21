54247a6185369ac0523de9c219752e1715d6979f
pizza-init <s.e.hmissi@gmail.com>
2026-03-21 01:02:48 -0400

    chore: Nightly build 1.6.0-nightly.883116a

--- include/ui_common.h
+++ include/ui_common.h
@@ -19,7 +19,7 @@
 #define DEFAULT_WIFI_PASSWORD ""
 
 // Firmware version
-#define FIRMWARE_VERSION "1.6.0-nightly.85c1437"
+#define FIRMWARE_VERSION "1.6.0-nightly.883116a"
 #define GITHUB_REPO "OpenSurface/SonosESP"
 #define GITHUB_API_URL "https://api.github.com/repos/" GITHUB_REPO "/releases/latest"
 
